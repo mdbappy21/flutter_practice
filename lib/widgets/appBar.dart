e9fd344f41412707f610d71625d03ed35b5b3507
Md Bappy <mdbappy1350@gmail.com>
2024-04-18 18:13:16 +0600

    Separeted widgets from HomeActivity

---
+++ lib/widgets/appBar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'snackBar.dart';
+
+class appBar extends StatelessWidget implements PreferredSizeWidget{
+  @override
+  Widget build(BuildContext context) {
+   return AppBar(
+     title: Text("Ostad Practice"),
+     titleSpacing: 0,
+     centerTitle: true,
+     toolbarHeight: 60,
+     toolbarOpacity: 1,
+     elevation: 6,
+     backgroundColor: Colors.deepOrange,
+     foregroundColor: Colors.black,
+     actions: [
+       IconButton(
+           onPressed: () {
+             snackBar.mySnackBar("Search Successfully done", context,
+                 duration: 500);
+           },
+           icon: Icon(Icons.search_rounded))
+     ],
+   );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(60);
+}
---
+++ lib/widgets/bottomNavigationBar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'snackBar.dart';
+
+class bottomNavigationBar extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.email_rounded), label: "Email"),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
+      ],
+      onTap: (int index) {
+        if (index == 0) {
+          snackBar.mySnackBar("Home bottom menu", context);
+        }
+        if (index == 1) {
+          snackBar.mySnackBar("Email bottom menu", context);
+        }
+        if (index == 2) {
+          snackBar.mySnackBar("Profile bottom menu", context);
+        }
+      },
+    );
+  }
+}
