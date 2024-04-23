c9df3c2b3ecfe9fbb8c0754474df8db87820b1ab
Md Bappy <mdbappy1350@gmail.com>
2024-04-23 23:35:17 +0600

    Practice

--- lib/Screens/HomeActivity.dart
+++ lib/Screens/HomeActivity.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/Screens/SecondScreen.dart';
 import 'package:flutter_practice/widgets/AppDrawer.dart';
+import 'package:flutter_practice/widgets/newScreenNevigation.dart';
+import 'package:flutter_practice/widgets/snackBar.dart';
 import '../widgets/FAButton.dart';
 import '../widgets/appBar.dart';
+import '../widgets/snackBar.dart';
 import '../widgets/bottomNavigationBar.dart';
 
 class HomeActivity extends StatelessWidget {
@@ -9,33 +13,69 @@ class HomeActivity extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      backgroundColor: Colors.grey,
-      appBar: appBar(),
-      floatingActionButton: const FAButton(),
-      bottomNavigationBar: bottomNavigationBar(),
-      drawer: AppDrawer(),
-      endDrawer: AppDrawer(),
-      body: const Center(
-        child:Column(
-          children: [
-
-          ],
-        ),
-      )
-
-      // const Center(
-      //   child: Text(
-      //     "Hello Everyone .\nWelcome to my new Journey.",
-      //     textAlign: TextAlign.center,
-      //     maxLines: 2,
-      //     style: TextStyle(
-      //         fontSize: 20,
-      //         fontWeight: FontWeight.w700,
-      //         overflow: TextOverflow.ellipsis),
-      //   ),
-      // ),
-
-    );
+        backgroundColor: Colors.grey.shade300,
+        appBar: appBar(title: 'Ostad Practice',),
+        floatingActionButton: const FAButton(),
+        bottomNavigationBar: bottomNavigationBar(),
+        drawer: AppDrawer(),
+        endDrawer: AppDrawer(),
+        body:  Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            //mainAxisSize: MainAxisSize.min,
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Hello Everyone."),
+                  Text("Welcome To my first application."),
+                  // Text(
+                  //   "Hello Everyone .\nWelcome to my new Journey.",
+                  //   textAlign: TextAlign.center,
+                  //   maxLines: 2,
+                  //   style: TextStyle(
+                  //       fontSize: 20,
+                  //       fontWeight: FontWeight.w700,
+                  //       overflow: TextOverflow.ellipsis),
+                  // ),
+                ],
+              ),
+             const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text("This application for "),
+                  Text("Practice purpose."),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text("Hi ,"),
+                      Text("their "),
+                      Text("I am Bappy "),
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      newScreenNevigation.navigateToNextScreen(context,SecondScreen());
+                    },
+                    child: const Text('Next Screen'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/widgets/snackBar.dart
+++ lib/widgets/snackBar.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class snackBar {
-  static void mySnackBar(String massage,BuildContext context,{int duration=500}) {
+  static void mySnackBar(String massage,BuildContext context,{int duration=500,Color? backgroundColor=Colors.blue}) {
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       content: Text(massage),
       duration: Duration(milliseconds: duration), // Change duration here
+      backgroundColor: backgroundColor,
     ));
   }
 }
