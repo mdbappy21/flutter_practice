21eb98bd7fb686329c5b45894dc8f861ae6a5ca4
Md Bappy <mdbappy21.cse@gmail.com>
2024-04-28 22:00:04 +0600

    Practice

---
+++ lib/Screens/FourthScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice/widgets/appBar.dart';
+
+class FourthScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(title: "Grid View",),
+    );
+  }
+
+}
--- lib/Screens/ThirdScreen.dart
+++ lib/Screens/ThirdScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
+import '../widgets/newScreenNevigation.dart';
 import '../widgets/appBar.dart';
+import 'FourthScreen.dart';
 
 class ThirdScreen extends StatelessWidget {
   @override
@@ -8,6 +9,7 @@ class ThirdScreen extends StatelessWidget {
     return Scaffold(
       appBar: appBar(title: 'This is for TextField,Container'),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Padding(
             padding: const EdgeInsets.all(16),
@@ -41,62 +43,117 @@ class ThirdScreen extends StatelessWidget {
                     filled: true,
                     suffixIcon: Icon(Icons.remove_red_eye))),
           ),
-          // Padding(
-          //   padding: const EdgeInsets.all(16),
-          //   child: TextFormField(
-          //       style: const TextStyle(
-          //         fontSize: 24,
-          //         color: Colors.orange,
-          //       ),
-          //       maxLength: 15,
-          //       decoration: InputDecoration(
-          //           hintText: "Hello This is Text Field",
-          //           hintStyle: const TextStyle(fontWeight: FontWeight.w400),
-          //           labelText: "For Testing purpose",
-          //           labelStyle: const TextStyle(
-          //             color: Colors.black,
-          //             fontSize: 18,
-          //           ),
-          //           border: OutlineInputBorder(
-          //             borderRadius: BorderRadius.circular(8),
-          //             borderSide: BorderSide(width: 4),
-          //           ),
-          //           focusedBorder: OutlineInputBorder(
-          //             borderSide: BorderSide(width: 4, color: Colors.teal),
-          //             borderRadius: BorderRadius.circular(16),
-          //           ),
-          //           enabledBorder: OutlineInputBorder(
-          //             borderSide: BorderSide(width: 4, color: Colors.black),
-          //             borderRadius: BorderRadius.circular(25),
-          //           ),
-          //           fillColor: Colors.white,
-          //           filled: true,
-          //           suffixIcon: Icon(Icons.remove_red_eye)
-          //       ),
-          //   ),
-          // ),
-          Container(
-            child: Text("This is Container"),
-            width: 300,
-            height: 300,
-
-            alignment: Alignment.topCenter,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(16),
-              border: Border.all(color: Colors.black,width: 2,),
-              // border:const Border(
-              //   top: BorderSide(color: Colors.black,width: 2),
-              //   left: BorderSide(color: Colors.black,width: 2),
-              //   right: BorderSide(color: Colors.black,width: 2),
-              //   bottom: BorderSide(color: Colors.black,width: 2),
-              // )
-              image: const DecorationImage(
-                image: NetworkImage("https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_640.jpg"),
-                fit: BoxFit.cover
-              )
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextFormField(
+                style: const TextStyle(
+                  fontSize: 24,
+                  color: Colors.orange,
+                ),
+                maxLength: 15,
+                decoration: InputDecoration(
+                    hintText: "Hello This is Text Field",
+                    hintStyle: const TextStyle(fontWeight: FontWeight.w400),
+                    labelText: "For Testing purpose",
+                    labelStyle: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(width: 4),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 4, color: Colors.teal),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(width: 4, color: Colors.black),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    suffixIcon: Icon(Icons.remove_red_eye)
+                ),
             ),
           ),
+          Row(
+            children: [
+              Container(
+                child: Text("This is Container", style: TextStyle(color: Colors.lightGreenAccent),),
+                width: 180,
+                height: 180,
+                alignment: Alignment.topCenter,
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                    // border:const Border(
+                    //   top: BorderSide(color: Colors.black,width: 2),
+                    //   left: BorderSide(color: Colors.black,width: 2),
+                    //   right: BorderSide(color: Colors.black,width: 2),
+                    //   bottom: BorderSide(color: Colors.black,width: 2),
+                    // )
+                    image: const DecorationImage(
+                        image: NetworkImage(
+                            "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_640.jpg"),
+                        fit: BoxFit.cover),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
+                      )
+                    ]),
+              ),Container(
+                child: Text("This is Container", style: TextStyle(color: Colors.lightGreenAccent),),
+                width: 180,
+                height: 180,
+                alignment: Alignment.topCenter,
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                    // border:const Border(
+                    //   top: BorderSide(color: Colors.black,width: 2),
+                    //   left: BorderSide(color: Colors.black,width: 2),
+                    //   right: BorderSide(color: Colors.black,width: 2),
+                    //   bottom: BorderSide(color: Colors.black,width: 2),
+                    // )
+                    image: const DecorationImage(
+                        image: NetworkImage(
+                            "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_640.jpg"),
+                        fit: BoxFit.cover),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
+                      )
+                    ]),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  newScreenNevigation.navigateToNextScreen(
+                      context, FourthScreen());
+                },
+                child: const Text('Next Screen Text Field'),
+              ),
+            ],
+          )
         ],
       ),
     );
