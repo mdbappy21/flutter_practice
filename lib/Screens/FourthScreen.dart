b653dd6f45ec7c68931fd5c3732d8029987572f3
Md Bappy <mdbappy1350@gmail.com>
2024-05-02 23:48:47 +0600

    Practice

--- lib/Screens/FourthScreen.dart
+++ lib/Screens/FourthScreen.dart
@@ -1,62 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/widgets/appBar.dart';
 
-class FourthScreen extends StatelessWidget{
+class FourthScreen extends StatelessWidget {
+  List<String> StudentName = [
+    "Bappy",
+    "Akash",
+    "Shuvo",
+    "Abdullah",
+    "Sabbir",
+    "Auntu"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(title: "List View",),
-      body: ListView(
-        children: const [
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-          Text("Hello"),
-        ],
+      backgroundColor: Colors.grey.shade50,
+      appBar: appBar(
+        title: "List View",
+      ),
+      body: ListView.builder(
+        itemCount: StudentName.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              textAlign: TextAlign.center,
+              "${StudentName[index]}",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w900),
+            ),
+          );
+        },
+        // children: const [
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //   // Text("Hello"),
+        //
+        // ],
       ),
     );
   }
-
 }
