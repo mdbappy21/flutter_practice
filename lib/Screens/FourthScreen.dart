995f76e3e7cefaf1aece4d106334a513be0cca66
Md Bappy <mdbappy1350@gmail.com>
2024-05-03 22:51:20 +0600

    Practice

--- lib/Screens/FourthScreen.dart
+++ lib/Screens/FourthScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/widgets/appBar.dart';
+import '../widgets/newScreenNevigation.dart';
+import 'FifthScreen.dart';
 
 class FourthScreen extends StatelessWidget {
   List<String> StudentName = [
@@ -14,67 +16,113 @@ class FourthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade50,
-      appBar: appBar(
-        title: "List View",
-      ),
-      body: ListView.builder(
-        itemCount: StudentName.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              textAlign: TextAlign.center,
-              "${StudentName[index]}",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w900),
+        backgroundColor: Colors.grey.shade50,
+        appBar: appBar(
+          title: "List View",
+        ),
+        body: Column(
+          children: [
+            ListView.separated(
+              shrinkWrap: true,
+              separatorBuilder: (context, index) {
+                return Divider(
+                  height: 20,
+                  thickness: 2,
+                  indent: 16,
+                  endIndent: 16,
+                  color: Colors.amber,
+                );
+              },
+              itemCount: StudentName.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Text(
+                      "${StudentName[index]}",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ],
+                );
+              },
             ),
-          );
-        },
-        // children: const [
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //   // Text("Hello"),
-        //
-        // ],
-      ),
-    );
+            SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber,
+                foregroundColor: Colors.black
+              ),
+              onPressed: () {
+                newScreenNevigation.navigateToNextScreen(
+                    context, FifthScreen());
+              },
+              child: const Text('Next Screen Grid View'),
+            ),
+          ],
+        ));
   }
 }
+// body: ListView.builder(
+//   itemCount: StudentName.length,
+//   itemBuilder: (BuildContext context, int index) {
+//     return Padding(
+//       padding: const EdgeInsets.all(8.0),
+//       child: Column(
+//         children: [
+//           Text(
+//             textAlign: TextAlign.center,
+//             "${StudentName[index]}",
+//             style: TextStyle(fontSize: 18, fontWeight: FontWeight.w900),
+//           ),
+//           Divider()
+//         ],
+//       ),
+//     );
+//   },
+//   // children: const [
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //   // Text("Hello"),
+//   //
+//   // ],
+// ),
