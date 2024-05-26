681bb15cbcd6925e721c7e7c4d3a7d38b73859ad
Md Bappy <mdbappy1350@gmail.com>
2024-05-26 23:56:06 +0600

    Practice

---
+++ lib/Screens/SeventhScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice/widgets/appBar.dart';
+
+class SeventhSecreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(title: 'For Theme',),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+
+}
